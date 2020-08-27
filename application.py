d2395ddc38b2669f720e0e7abaaff9426a1a3cc6
Daniel Chif <nadchif@gmail.com>
2020-08-27 23:49:39 +0200

    bugfix: fixed CORS issues

--- application.py
+++ application.py
@@ -5,9 +5,11 @@ import flask
 from flask import request, jsonify
 from torchvision import models, transforms
 from PIL import Image
+from flask_cors import CORS
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+cors = CORS(app)
 
 
 @app.route('/', methods=['GET'])
